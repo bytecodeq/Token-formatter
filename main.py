e5d7841436cdf509e259e06c21d382df5a4064e2
bytecode <lazarcasiian@gmail.com>
2025-01-14 18:09:19 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+def Formatter() -> None:
+    success = 0
+    failure = 0
+    
+    with open("input/tokens.txt", "r") as input_file, open("output/formatted_tokens.txt", "w") as output_file:
+        for line in input_file:
+            line = line.strip()
+            
+            if ":" in line:
+                token_parts = line.split(":")
+                new_token = token_parts[-1] + "\n"
+                output_file.write(new_token)
+                success += 1
+                print(f"formatted {line[:25]}")
+                time.sleep(0.0001)
+
+            else:
+                output_file.write(line + "\n")
+                failure += 1
+                print("failed  formatting")
+                time.sleep(0.0001)
+
+    print(f"succes: {succes} tokens")
+    print(f"failure: {failure}  tokens")
+
+if __name__ == "__main__":
+    Formatter()
